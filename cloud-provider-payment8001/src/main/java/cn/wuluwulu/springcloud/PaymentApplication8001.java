880f5f36eba98a86f1f54e7ab055142b47e47373
humeng <humeng_mail@163.com>
2023-05-15 22:29:42 +0800

    update:支付模块初始化

---
+++ cloud-provider-payment8001/src/main/java/cn/wuluwulu/springcloud/PaymentApplication8001.java
@@ -0,0 +1,11 @@
+package cn.wuluwulu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApplication8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication8001.class, args);
+    }
+}
