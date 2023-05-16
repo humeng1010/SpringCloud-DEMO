33ad5e9c86fa11f169a2350fac4a6826d4cbc60e
humeng <humeng_mail@163.com>
2023-05-16 14:38:13 +0800

    update:支付模块运行测试通过

--- cloud-provider-payment8001/src/main/java/cn/wuluwulu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/cn/wuluwulu/springcloud/controller/PaymentController.java
@@ -3,12 +3,14 @@ package cn.wuluwulu.springcloud.controller;
 import cn.wuluwulu.springcloud.entity.CommonResult;
 import cn.wuluwulu.springcloud.entity.Payment;
 import cn.wuluwulu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/payment")
+@Slf4j
 public class PaymentController {
 
     @Autowired
@@ -17,11 +19,14 @@ public class PaymentController {
 
     @PostMapping("/create")
     public CommonResult<Integer> create(@RequestBody Payment payment) {
+        log.info("创建支付");
         return paymentService.create(payment);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        log.info("查询支付");
+        log.info("ok");
         return paymentService.getPaymentById(id);
     }
 
