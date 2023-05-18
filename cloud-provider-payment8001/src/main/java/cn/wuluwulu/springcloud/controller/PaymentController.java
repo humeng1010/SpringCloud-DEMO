b0a4e9b6c237c77eb5579bfde940c4dadbff4fd9
humeng <humeng_mail@163.com>
2023-05-18 08:59:11 +0800

    update:Eureka服务发现,Eureka自我保护

--- cloud-provider-payment8001/src/main/java/cn/wuluwulu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/cn/wuluwulu/springcloud/controller/PaymentController.java
@@ -6,7 +6,12 @@ import cn.wuluwulu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/payment")
@@ -16,6 +21,9 @@ public class PaymentController {
     @Autowired
     @Qualifier("paymentServiceImpl")
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/create")
     public CommonResult<Integer> create(@RequestBody Payment payment) {
@@ -28,6 +36,26 @@ public class PaymentController {
         log.info("查询支付");
         log.info("ok");
         return paymentService.getPaymentById(id);
+    }
+
+    /**
+     * 服务发现
+     *
+     * @return discovery
+     */
+    @GetMapping("/discovery")
+    public Object getDiscovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("******service:{}", service);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+
+        return this.discoveryClient;
     }
 
 }
