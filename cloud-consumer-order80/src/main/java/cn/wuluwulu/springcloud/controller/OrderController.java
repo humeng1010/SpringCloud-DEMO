f85fdfd301652cea3cd6048c297980d19bb44fd9
humeng <humeng_mail@163.com>
2023-05-19 15:54:20 +0800

    update:Ribbon基础知识RestTemplate的使用:getForObject,getForEntity,postForObject,postForEntity

--- cloud-consumer-order80/src/main/java/cn/wuluwulu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/cn/wuluwulu/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package cn.wuluwulu.springcloud.controller;
 import cn.wuluwulu.springcloud.entity.CommonResult;
 import cn.wuluwulu.springcloud.entity.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +37,28 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> responseEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        //
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return responseEntity.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败");
+        }
+
+    }
+
+    @GetMapping("/consumer/paymentForEntity/create")
+    public CommonResult<Payment> create2(Payment payment) {
+        ResponseEntity<CommonResult> responseEntity = restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return responseEntity.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败");
+        }
     }
 
 }
