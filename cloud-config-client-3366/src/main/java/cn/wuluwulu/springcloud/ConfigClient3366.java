06294abe0cc2860645540be344c50b911a74a279
humeng <humeng_mail@163.com>
2023-08-01 20:17:37 +0800

    update: 引入消息队列RabbitMQ,由统一的一个配置中心通过消息队列通知多个不同的client客户端刷新配置

---
+++ cloud-config-client-3366/src/main/java/cn/wuluwulu/springcloud/ConfigClient3366.java
@@ -0,0 +1,13 @@
+package cn.wuluwulu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClient3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3366.class, args);
+    }
+}
