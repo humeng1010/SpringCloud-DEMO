8456dc3f6570ee130b1eaac18adf7c98bdb5ba10
humeng <humeng_mail@163.com>
2023-08-01 17:42:16 +0800

    update: 配置中心服务端(读取github仓库)+客户端(读取注册中心的服务端,手动发送POST请求刷新配置)

---
+++ cloud-config-center3344/src/main/java/cn/wuluwulu/springcloud/ConfigCenter3344.java
@@ -0,0 +1,13 @@
+package cn.wuluwulu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenter3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenter3344.class, args);
+    }
+}
---
+++ cloud-config-client3355/src/main/java/cn/wuluwulu/springcloud/ConfigClient3355.java
@@ -0,0 +1,13 @@
+package cn.wuluwulu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClient3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3355.class, args);
+    }
+}
